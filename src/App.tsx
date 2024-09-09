705633d80abdccacf2ad634d0640767fea4bea8b
zac <yeongi@deeplehr.com>
2024-09-09 13:38:31 +0900

    feat 자소서 sql 생성기

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,124 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-
-// INSERT INTO jd_cover_letter (jd_id, subject, content_length, created_time, updated_time) VALUES
-// (27008, '자소서 문항1', 1000, NOW(), NULL),
-//   (27008, '자소서 문항2', 1000, NOW(), NULL),
-//   (27008, '자소서 문항3', 1000, NOW(), NULL);
-//
-// ### 작성 가이드
-//
-// - 1번째 칸
-// - 공고의 ID
-// - [https://고초대졸.com/jd/detail/27008](https://xn--299a59id5upfe.com/jd/detail/27008)
-// - 위 링크에서 맨 뒤에 숫자입니다.
-// - 2번째 칸
-// - 자소서 문항
-// - 3번째 칸
-// - 최대 글자 수입니다.
-// - 글자 수 제한이 없다면 `NULL` 로 입력합니다.
-//
-//   문항이 N개라면 `(27008, '자소서 문항1', 1000, NOW(), NULL)` 해당 내용을 N줄 작성합니다.
-//
-//   마지막 줄 맨 끝엔 `;` 기호를 붙이고 그 외엔 `,` 기호를 붙입니다.
+type CoverLetterItemFormValue = {
+  subject: string;
+  contentLength?: string;
+}
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [coverLetters, setCoverLetters] = useState<CoverLetterItemFormValue[]>([]);
+
+  const [jdId, setJdId] = useState("");
+  const [subject, setSubject] = useState("")
+  const [contentLength, setContentLength] = useState("");
+
+  const handleMakeSQL = () => {
+
+    if(!coverLetters) {
+      return '';
+    }
+
+    if(coverLetters?.length === 1) {
+      return `
+      INSERT INTO jd_cover_letter (jd_id, subject, content_length, created_time, updated_time) VALUES
+      ${jdId}, '${coverLetters[0].subject}', ${coverLetters[0].contentLength || "NULL"}, NOW(), NULL);
+      `;
+    }
+
+    return `
+    INSERT INTO jd_cover_letter (jd_id, subject, content_length, created_time, updated_time) VALUES
+    ${
+      coverLetters.map((coverLetter, index) => {
+        return `(${jdId}, '${coverLetter.subject}', ${coverLetter.contentLength || "NULL"}, NOW(), NULL)${index === coverLetters.length - 1 ? ';' : ','}`
+      }).join('\n')
+    }
+    `;
+  }
+
+  const handleCopySQL = () => {
+    const sql = handleMakeSQL();
+    navigator.clipboard.writeText(sql);
+    alert("SQL 문이 복사되었습니다");
+  }
+
+  const handleAddCoverLetter = () => {
+
+    if(jdId === "") {
+      alert("공고 Id를 입력해주세요.");
+      return;
+    }
+
+    if(subject === "") {
+      alert("자소서 문항을 입력해주세요.");
+      return;
+    }
+
+    setCoverLetters([...coverLetters, {
+      subject,
+      contentLength,
+    }]);
+    setContentLength("");
+    setSubject("");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <h2>
+        자소서 항목 만들기
+      </h2>
+      <section>
+        <button onClick={handleCopySQL}>
+          SQL 문 복사하기
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </section>
+      <section>
+        <article>
+          <p>
+            자소서 항목을 입력하시면 SQL 문이 생성됩니다.
+          </p>
+          <label>
+            <p>공고 Id</p>
+            <input value={jdId} onChange={(e)=> setJdId(e.target.value)}/>
+          </label>
+              <div className="form">
+                <label>
+                  <p>자소서 문항</p>
+                  <textarea
+                    id="subject"
+                    className="subject"
+                    value={subject} onChange={e=>setSubject(e.target.value)}/>
+                </label>
+                <label>
+                  <p>최대 글자 수</p>
+                  <input
+                    id="contentLength"
+                    type="number"
+                    placeholder="최대 글자 수(없으면 공란)"
+                    value={contentLength}
+                    max={10000}
+                    onChange={e => setContentLength(e.target.value)}
+                    />
+                </label>
+                <button onClick={handleAddCoverLetter}>
+                  항목 추가
+                </button>
+              </div>
+        </article>
+
+        <article>
+          <div>
+          <h2>결과</h2>
+            <p className="result">
+              {handleMakeSQL()}
+            </p>
+          </div>
+        </article>
+      </section>
+
+    </div>
   )
 }
 
